9d5cff1ab9f409a6d9703c3ba04010bc8fb06b64
MinganB <60131801+MinganB@users.noreply.github.com>
2024-08-17 14:02:53 +0200

    Added onboarding datacapture views

--- app/Views/onboarding/welcome.php
+++ app/Views/onboarding/welcome.php
@@ -1,6 +1,6 @@
-<div class="container d-flex justify-content-center align-items-center min-vh-100">
+<div class="min-height-80 container d-flex justify-content-center align-items-center my-4">
     <div class="text-center">
-        <img src="placeholder-image.jpg" alt="Placeholder Image" class="img-fluid mb-4">
+        <img src="<?= base_url('img/welcome.jpg') ?>" alt="Welcome" width="300px" class="img-fluid mb-4">
         <h1 class="mb-3">Welcome to <?= APP_NAME ?></h1>
         <p class="lead text-muted mb-4">Before we begin, we need to ask you a few questions to help us understand your financial situation. This ensures we have the latest data relevant to your circumstances.</p>
         <a href="<?= base_url('me/onboarding/update') ?>" class="btn btn-primary btn-lg">Get Started</a>

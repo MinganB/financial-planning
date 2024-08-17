dd6b57a3a227e6438b9e37d93c4b9de18cb02464
MinganB <60131801+MinganB@users.noreply.github.com>
2024-08-17 11:40:49 +0200

    Added ShieldAuth library

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'donotreply@financial-planning.co.za';
+    public string $fromName   = 'Financial Planning App';
     public string $recipients = '';
 
     /**
